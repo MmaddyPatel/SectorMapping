d7023a3c2d227ae52989a9f0a6308e733e9c9976
MmaddyPatel <mukesh1.patel@zeebiz.com>
2024-07-03 10:02:07 +0530

    initial commit

---
+++ SectorMapping/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace SectorMapping
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstSector = new System.Windows.Forms.ListBox();
+            this.lst_stocks = new System.Windows.Forms.ListBox();
+            this.btn_add = new System.Windows.Forms.Button();
+            this.txtsearch = new System.Windows.Forms.TextBox();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.radNSE = new System.Windows.Forms.RadioButton();
+            this.radBSE = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // lstSector
+            // 
+            this.lstSector.FormattingEnabled = true;
+            this.lstSector.Location = new System.Drawing.Point(40, 40);
+            this.lstSector.Name = "lstSector";
+            this.lstSector.Size = new System.Drawing.Size(234, 511);
+            this.lstSector.TabIndex = 0;
+            this.lstSector.SelectedIndexChanged += new System.EventHandler(this.lstSector_SelectedIndexChanged);
+            // 
+            // lst_stocks
+            // 
+            this.lst_stocks.FormattingEnabled = true;
+            this.lst_stocks.Location = new System.Drawing.Point(963, 288);
+            this.lst_stocks.Name = "lst_stocks";
+            this.lst_stocks.Size = new System.Drawing.Size(234, 238);
+            this.lst_stocks.TabIndex = 1;
+            // 
+            // btn_add
+            // 
+            this.btn_add.Location = new System.Drawing.Point(844, 162);
+            this.btn_add.Name = "btn_add";
+            this.btn_add.Size = new System.Drawing.Size(107, 57);
+            this.btn_add.TabIndex = 3;
+            this.btn_add.Text = ">>>";
+            this.btn_add.UseVisualStyleBackColor = true;
+            this.btn_add.Click += new System.EventHandler(this.btn_add_Click);
+            // 
+            // txtsearch
+            // 
+            this.txtsearch.Location = new System.Drawing.Point(368, 16);
+            this.txtsearch.Name = "txtsearch";
+            this.txtsearch.Size = new System.Drawing.Size(133, 20);
+            this.txtsearch.TabIndex = 4;
+            this.txtsearch.TextChanged += new System.EventHandler(this.txtsearch_TextChanged);
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(289, 40);
+            this.checkedListBox1.MultiColumn = true;
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(521, 514);
+            this.checkedListBox1.TabIndex = 5;
+            this.checkedListBox1.ThreeDCheckBoxes = true;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(946, 40);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(217, 202);
+            this.richTextBox1.TabIndex = 6;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1021, 552);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(63, 31);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Save";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // radNSE
+            // 
+            this.radNSE.AutoSize = true;
+            this.radNSE.Location = new System.Drawing.Point(53, 13);
+            this.radNSE.Name = "radNSE";
+            this.radNSE.Size = new System.Drawing.Size(47, 17);
+            this.radNSE.TabIndex = 8;
+            this.radNSE.TabStop = true;
+            this.radNSE.Text = "NSE";
+            this.radNSE.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.radNSE.UseVisualStyleBackColor = true;
+            this.radNSE.CheckedChanged += new System.EventHandler(this.radNSE_CheckedChanged);
+            // 
+            // radBSE
+            // 
+            this.radBSE.AutoSize = true;
+            this.radBSE.Location = new System.Drawing.Point(144, 13);
+            this.radBSE.Name = "radBSE";
+            this.radBSE.Size = new System.Drawing.Size(46, 17);
+            this.radBSE.TabIndex = 9;
+            this.radBSE.TabStop = true;
+            this.radBSE.Text = "BSE";
+            this.radBSE.UseVisualStyleBackColor = true;
+            this.radBSE.CheckedChanged += new System.EventHandler(this.radBSE_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1241, 600);
+            this.Controls.Add(this.radBSE);
+            this.Controls.Add(this.radNSE);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.txtsearch);
+            this.Controls.Add(this.btn_add);
+            this.Controls.Add(this.lst_stocks);
+            this.Controls.Add(this.lstSector);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstSector;
+        private System.Windows.Forms.ListBox lst_stocks;
+        private System.Windows.Forms.Button btn_add;
+        private System.Windows.Forms.TextBox txtsearch;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton radNSE;
+        private System.Windows.Forms.RadioButton radBSE;
+    }
+}
+
---
+++ SectorMapping/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Odbc;
+using System.Data.OleDb;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ZEE.DAL;
+namespace SectorMapping
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            BindSector();
+            BindStocks();
+        }
+
+        private void BindSector()
+        {
+            DataTable dt;
+            string str = "";
+            str = "exec sp_get_sector";
+            dt = SqlHelper.ExecuteDataset(Properties.Settings.Default.conn_str_main, CommandType.Text, str).Tables[0];
+
+            lstSector.DataSource = dt;
+            lstSector.DisplayMember = "only_sector_name";
+            lstSector.ValueMember = "sector_id";
+
+
+        }
+   private void BindStocks()
+        {
+            DataTable dt;
+            string str = "";
+            int exchange = 1;
+            if (radBSE.Checked == true)
+            {
+                exchange = 2;
+            }
+            str = "sp_get_companyList " + exchange;
+            dt = SqlHelper.ExecuteDataset(Properties.Settings.Default.conn_str_main, CommandType.Text, str).Tables[0];
+
+            checkedListBox1.DataSource = dt;
+            checkedListBox1.DisplayMember = "abbrs";
+            checkedListBox1.ValueMember = "stock_id";
+
+
+        }
+        private void lstSector_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            BindSectorList();
+
+        }
+
+        private void BindSectorList()
+        {
+            string selectedText = lstSector.GetItemText(lstSector.SelectedItem);
+            string selectedValue = lstSector.SelectedValue.ToString();
+            int exchange = 1;
+            if (radBSE.Checked == true)
+            {
+                exchange = 2;
+            }
+            string str = "SP_GET_SORTED_INTRADAY  "+exchange +",'TOP TRADES','" + selectedValue + "','50'";
+            DataTable dt = SqlHelper.ExecuteDataset(Properties.Settings.Default.conn_str_main, CommandType.Text, str).Tables[0];
+
+            lst_stocks.DataSource = dt;
+            lst_stocks.DisplayMember = "name";
+            lst_stocks.ValueMember = "stock_id";
+        }
+        public class ListItem
+        {
+            public string Name { get; set; }
+            public string Value { get; set; }
+
+            public override string ToString()
+            {
+                return Name;
+            }
+        }
+        private void btn_add_Click(object sender, EventArgs e)
+        {
+            foreach (var item in checkedListBox1.CheckedItems)
+            {
+                var row = (item as DataRowView).Row;
+                richTextBox1.AppendText(row["abbrs"].ToString() + " | " + row["stock_id"].ToString() + "\r\n");
+            }
+            
+          
+        }
+
+        private void txtsearch_TextChanged(object sender, EventArgs e)
+        {
+       
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+         
+              
+
+            foreach (var item in checkedListBox1.CheckedItems)
+            {
+                var row = (item as DataRowView).Row;
+                string selectedValue = lstSector.SelectedValue.ToString();
+                string str = "update stock_items set sector_id =isnull(SECTOR_ID,'') + ',"+ selectedValue + ",' where stock_id ='"+ row["stock_id"].ToString() + "'";
+                SqlHelper.ExecuteNonQuery(Properties.Settings.Default.conn_str_main, CommandType.Text, str);
+            }
+            richTextBox1.Text = "";
+
+
+            for (int i = 0; i < checkedListBox1.Items.Count; i++)
+            {
+                checkedListBox1.SetItemChecked(i, false);
+            }
+            BindSectorList();
+           
+        }
+
+        private void radNSE_CheckedChanged(object sender, EventArgs e)
+        {
+            BindSector();
+            BindStocks();
+        }
+
+        private void radBSE_CheckedChanged(object sender, EventArgs e)
+        {
+            BindSector();
+            BindStocks();
+        }
+    }
+}
